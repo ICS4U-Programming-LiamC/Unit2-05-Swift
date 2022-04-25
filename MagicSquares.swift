f029264beaeaa1a35527801d7af71b79f6a2db9a
liam-csiffary <liam.csiffary@stu.ocsb.ca>
2022-04-25 02:07:25 +0000

    finished!

--- MagicSquares.swift
+++ MagicSquares.swift
@@ -2,50 +2,74 @@
 //  MagicSquares.swift
 //
 //  Created by Liam Csiffary
-//  Created on 2022-04-22
-//  Version 1.0
+//  Created on 2022-04-24
+//  Version 1.1
 //  Copyright (c) 2022 IMH. All rights reserved.
 //
 //  MagicNumbers.java brute force algorithm,
 //  generates all possible 3x3's populated with numbers 1-9
-//  then deletes the ones that are not magic squares
+//  then sends each square to the "isMagicSquare"
+// function, which prints the square that are magic
 
+import Foundation
 
-
-
-
-extension Array {
-    var permutations: [[Element]] {
-        guard let (head, rest) = self.chopped() else { return [[]] }
-        return rest.permutations.flatMap { $0.interleaved(head) }
-    }
+// permutation function
+// https://stackoverflow.com/questions/34968470/calculate-all-permutations-of-a-string-in-swift
+func permutations(_ n:Int, _ a: inout Array<Character>) {
+  // the {isMagicSquare(arrString: a); is all I did
+  if n == 1 {isMagicSquare(arrString: a); return}
+  for i in 0..<n-1 {
+    permutations(n-1,&a)
+    a.swapAt(n-1, (n%2 == 1) ? 0 : i)
+  }
+  permutations(n-1,&a)
 }
 
-  func isMagicSquare(array: [Int]) -> [Int] {
-    // calculates what all the parts of the square add up to
-    let row1 = array[0] + array[1] + array[2]
-    let row2 = array[3] + array[4] + array[5]
-    let row3 = array[6] + array[7] + array[8]
-    let column1 = array[0] + array[1] + array[2]
-    let column2 = array[0] + array[1] + array[2]
-    let column3 = array[0] + array[1] + array[2]
-    let diagonal1 = array[0] + array[1] + array[2]
-    let diagonal2 = array[0] + array[1] + array[2]
-    
+// function to check if it is a magic square
+func isMagicSquare(arrString: Array<Character>) {
 
+  // turns the array of chars into an array of ints
+  var arrInt: [Int] = []
 
-    if ((row1 == row2) && (row2 == row3) && (row3 == column1) && (column1 == column2)
-        && (column2 == column3) && (column3 == diagonal1) && (diagonal1 == diagonal2)) {
-      
-      // if they all add up to the same number add the array to the list
-      correctSquares.add(allPossibleSquares.get(i));
-    }
+  for each in arrString {
+    arrInt.append(Int(String(each)) ?? -1)
   }
+
+  // gets the totals of all the combinations
+  let row1 = arrInt[0] + arrInt[1] + arrInt[2]
+  let row2 = arrInt[3] + arrInt[4] + arrInt[5]
+  let row3 = arrInt[6] + arrInt[7] + arrInt[8]
+  let column1 = arrInt[0] + arrInt[3] + arrInt[6]
+  let column2 = arrInt[1] + arrInt[4] + arrInt[7]
+  let column3 = arrInt[2] + arrInt[5] + arrInt[8]
+  let diagonal1 = arrInt[0] + arrInt[4] + arrInt[8]
+  let diagonal2 = arrInt[2] + arrInt[4] + arrInt[6]
+
+  // if they are all share a sum
+  if ((row1 == row2) && (row2 == row3) && (row3 == column1) && (column1 == column2)
+      && (column2 == column3) && (column3 == diagonal1) && (diagonal1 == diagonal2)) {
+
+    // print the array with formatting
+    var i = 0
+    for each in arrInt {
+    // for each in the array
+      if ((i + 1) % 3 == 0) {
+        print(each);
+      } else {
+        print(String(each) + ", ", terminator: " ");
+      }
+      i += 1
+    }
+    print()
+  }
+}
 
 // main function
 func main() {
-  let arrayOfPermutes = [1, 2, 3, 4, 5, 6, 7, 8, 9].permutations
-  print(arrayOfPermutes)
+  // create the array
+  var arr = Array("123456789")
+  // send the array to the permutation generator
+  permutations(9, &arr)
 }
 
 // start the program
